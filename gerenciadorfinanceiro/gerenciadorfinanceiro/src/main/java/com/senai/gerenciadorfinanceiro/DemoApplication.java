24a21e0cb6fd356aa7940aec9215fd3a586bf793
Rogerio Andre Queke Junior <junior_queke1@hotmail.com>
2020-06-18 19:37:53 -0300

    Correção do import na DemoApplication import do local estava incorreto. Adição de funcionalidades para fazer o conexão com o banco de dados no application.properties. Anotações no application.properties para retirar alguns warnings no debug console

--- gerenciadorfinanceiro/gerenciadorfinanceiro/src/main/java/com/senai/gerenciadorfinanceiro/DemoApplication.java
+++ gerenciadorfinanceiro/gerenciadorfinanceiro/src/main/java/com/senai/gerenciadorfinanceiro/DemoApplication.java
@@ -4,8 +4,8 @@ import java.util.Locale;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.servlet.WebMvcProperties.LocaleResolver;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 
 @SpringBootApplication 
